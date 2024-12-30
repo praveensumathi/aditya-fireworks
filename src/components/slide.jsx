cbaa18a4d40a7d3b6e41b6c3a55f4e6960045aff
KavipriyaLakshmanan <kavipriya.l@3analytics.com>
2024-12-30 22:23:02 +0530

    product page changes

--- src/components/slide.jsx
+++ src/components/slide.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Box } from "@mui/material";
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 const Slide = ({ items }) => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 2.5,
+    slidesToShow: 3.5,
     slidesToScroll: 1,
     autoplay: true,
+    centerMode: true, // Enable center mode
+    centerPadding: "0", // Ensure no padding on the left side
     responsive: [
       {
         breakpoint: 768,
@@ -23,17 +25,29 @@ const Slide = ({ items }) => {
   };
 
   return (
-    <Box className="w-full  m-auto">
+    <Box className="m-auto text-center">
       <Slider {...settings}>
         {items.map((item, index) => (
-          <div key={index} className="text-center">
-            <img
+          <Card key={index} sx={{ maxWidth: 345, boxShadow: 3, height: 600 }}>
+            <CardMedia
+              component="img"
               src={item.imageUrl}
               alt={item.title}
-              className="w-100 h-64 object-contain"
+              className="h-64 w-full object-cover p-4"
             />
-            <h3 className="mt-4 font-bold text-lg">{item.title}</h3>
-          </div>
+            <CardContent>
+              <Typography variant="h6" className="py-2 font-extrabold text-3xl">
+                {item.title}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className="py-2 text-justify "
+              >
+                {item.description}
+              </Typography>
+            </CardContent>
+          </Card>
         ))}
       </Slider>
     </Box>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,27 @@
 export const slider = [
-    { title: "Flower pots", imageUrl: "assets/flowerbots_slider.jpeg" },
-    { title: "Special flowerpots", imageUrl: "assets/special_flowerpots.webp" },
-    { title: "Chakkars", imageUrl: "assets/chakkars.webp" },
-    { title: "Special Chakkars", imageUrl: "assets/special_chakkars.webp" },
-    { title: "Lakshmi Bijili Crackers", imageUrl: "assets/lakshmi_crackers.jpeg" },
+    {
+        title: "Flower pots",
+        imageUrl: "assets/flowerbots_slider.jpeg",
+        description: `Our Flowerpots are a harmonious blend of vibrant colors and mesmerizing patterns, designed to light up the sky with elegance and grace. Handcrafted with precision and care, each Flowerpot showcases a symphony of hues that dance in the night, creating a breathtaking spectacle for all to admire. Whether it's a grand celebration or an intimate gathering, our Flowerpots add a touch of magic to every moment.`,
+    },
+    {
+        title: "Special flowerpots",
+        imageUrl: "assets/special_flowerpots.webp",
+        description: `Elevate your fireworks display with our exclusive collection of Special Flowerpots. These unique creations are crafted to perfection, combining artistic flair with spectacular effects that mesmerize and enchant. From cascading sparks to intricate patterns of light, each Special Flowerpot is a masterpiece designed to leave a lasting impression. Whether it's a wedding, a corporate event, or a private celebration, our Special Flowerpots add a touch of elegance and sophistication to any occasion.`,
+    },
+    {
+        title: "Chakkars",
+        imageUrl: "assets/chakkars.webp",
+        description: `Inspired by the traditional Indian sweet, our Chakkara crackers bring a delightful twist to fireworks displays. With their gentle crackling and sparkling effects, Chakkara crackers create a warm and inviting atmosphere, perfect for spreading joy and happiness. Crafted with care and attention to detail, each Chakkara cracker embodies the essence of traditional festivities and the joy of sharing moments with loved ones.`,
+    },
+    {
+        title: "Special Chakkars",
+        imageUrl: "assets/special_chakkars.webp",
+        description: `Experience the mesmerizing whirl of Special Chakkars. These unique fireworks spin and twirl with captivating grace, creating a stunning visual spectacle that captures the imagination. Crafted with precision and innovation, each Special Chakkar showcases a dazzling display of colors and patterns that enchant and delight. Whether it's a festive gathering or a special event, our Special Chakkars add an extra dimension of excitement and charm to the celebration.`,
+    },
+    {
+        title: "Lakshmi Bijili Crackers",
+        imageUrl: "assets/lakshmi_crackers.jpeg",
+        description: `Named after the Hindi word for lightning, our Bijili crackers illuminate the night with their intense brightness and crackling sounds. Designed to capture the essence of excitement and thrill, each Bijili cracker delivers a powerful burst of light and sound that leaves a lasting impression. Whether it's Diwali celebrations or festive gatherings, our Bijili crackers add a spark of joy and excitement to every occasion.`,
+    },
 ];
