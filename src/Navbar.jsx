b4dcdb535b00a7daafb9a343d0f11a2dbf1eb98a
praveenR <praveen.r@deventure.co>
2025-01-02 13:13:09 +0530

    color change

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,11 @@
-import { AppBar, Box, Button, Container, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+  useTheme,
+} from "@mui/material";
 import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import IconButton from "@mui/material/IconButton";
@@ -6,6 +13,8 @@ import IconButton from "@mui/material/IconButton";
 const pages = ["Home", "About", "Products", "Contact"];
 
 function Navbar() {
+  const theme = useTheme();
+
   return (
     <AppBar position="sticky">
       <div
@@ -24,58 +33,66 @@ function Navbar() {
           </p>
         </marquee>
       </div>
-      <Container maxWidth={false}>
-        <Toolbar disableGutters>
-          <a href="#home">
-            <Box
-              component="img"
+      <Toolbar sx={{ backgroundColor: "#091629" }}>
+        <a href="#home">
+          <Box
+            component="img"
+            sx={{
+              height: 80,
+            }}
+            alt="Your logo."
+            src={"assets/ADITYALOGO.png"}
+          />
+        </a>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "none", md: "flex" },
+            justifyContent: "flex-end",
+            gap: 5,
+          }}
+        >
+          {pages.map((page) => (
+            <Button
+              key={page}
+              href={`#${page.toLocaleLowerCase()}`}
+              //onClick={handleCloseNavMenu}
               sx={{
-                height: 70,
+                my: 2,
+                display: "block",
+                fontWeight: 800,
+                "&:hover": {
+                  backgroundColor: theme.palette.primary.main,
+                  color: "black",
+                },
               }}
-              alt="Your logo."
-              src={"assets/ADITYALOGO.png"}
-            />
-          </a>
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "none", md: "flex" },
-              justifyContent: "flex-end",
-              gap: 5,
-            }}
-          >
-            {pages.map((page) => (
-              <Button
-                key={page}
-                href={`#${page.toLocaleLowerCase()}`}
-                //onClick={handleCloseNavMenu}
-                sx={{ my: 2, display: "block", fontWeight: 800 }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "flex", md: "none" },
-              justifyContent: "flex-end",
-            }}
-          >
-            <IconButton
-              size="large"
-              aria-label="nav-menus"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              //onClick={handleOpenNavMenu}
-              color="inherit"
             >
-              <MenuIcon />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </Container>
+              {page}
+            </Button>
+          ))}
+        </Box>
+
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "flex", md: "none" },
+            justifyContent: "flex-end",
+          }}
+        >
+          <IconButton
+            size="large"
+            aria-label="nav-menus"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            //onClick={handleOpenNavMenu}
+            sx={{
+              color: theme.palette.primary.main,
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
+      </Toolbar>
     </AppBar>
   );
 }
